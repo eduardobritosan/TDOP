b963e8ab84bff11d0ba5251c9efca3608351afd4
Eduardo Brito Sanchez <eduardo_brito27@hotmail.com>
2015-03-25 19:03:16 +0000

    Creado gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,7 @@
+var gulp    = require('gulp'),
+    gutil   = require('gulp-util'),
+    uglify  = require('gulp-uglify'),
+    concat  = require('gulp-concat');
+var del     = require('del');
+var minifyHTML = require('gulp-minify-html');
+var minifyCSS  = require('gulp-minify-css');

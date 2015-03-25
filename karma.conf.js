ac73bdfb6fe8813a960c9aa0ec598bf63a663e5a
Eduardo Brito Sanchez <eduardo_brito27@hotmail.com>
2015-03-25 19:20:53 +0000

    Fallan los tests

--- karma.conf.js
+++ karma.conf.js
@@ -10,25 +10,37 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha'],
+    frameworks: ['mocha','chai'],
 
 
     // list of files / patterns to load in the browser
     files: [
+      'https://ajax.googleapis.com/ajax/libs/angularjs/1.0.5/angular.min.js',
+      'https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.5.2/underscore-min.js',
       '*.js',
       'tests/*.js'
     ],
 
+    client: {
+        mocha: {
+            ui: 'tdd'
+        }
+    },
+
 
     // list of files to exclude
     exclude: [
-      'gulpfile.js'
+      'gulpfile.js',
+      'tests/test.js',
+      'app.js'
     ],
 
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+         'index.html': ['html2js'],
     },
 
 
@@ -62,6 +74,6 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false
+    singleRun: true
   });
 };
